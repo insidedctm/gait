1141c849c357254ca886d3d4f1711592a4ef9012
Robin East <robin.east@xense.co.uk>
2019-01-21 13:56:35 +0000

    experiments with open-cv background subtraction

---
+++ human_mask.py
@@ -0,0 +1,24 @@
+import cv2
+import argparse
+
+def show_bg_subtraction(video_path):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    fgbg = cv2.bgsegm.createBackgroundSubtractorGSOC(mc=cv2.bgsegm.LSBP_CAMERA_MOTION_COMPENSATION_LK)
+
+    video = cv2.VideoCapture(video_path)
+    while True:
+      ok, frame = video.read()
+      if not ok:
+        break
+  
+      frame_bkg = fgbg.apply(frame)
+
+      cv2.imshow('background', frame_bkg)
+      cv2.waitKey(25)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Demonstrate background subtraction (using open-cv) on videos')
+    parser.add_argument('video_path', help='Video to process')
+    args = parser.parse_args()
+
+    show_bg_subtraction(args.video_path)
